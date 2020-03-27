52ae1950f40325f146931298ac4f42aadd6f0c59
洪远麒 <415453941@qq.com>
2020-03-27 12:21:25 +0800

    FIX

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const MYSQL = {
-  host: '127.0.0.1',
+  host: '',
   port: 3306,
-  username: 'root',
-  password: 'Admin.123',
-  database: 'bsus',
+  username: '',
+  password: '',
+  database: '',
 };
 
 export const QINIU = {
-  ak: '9G9jn3S47ZS1nMdYDvdqZSqbhEVIXI7F7Hv44eoz',
-  sk: 'nbCAx_Hg5GRSzs11jBGodU4Ihj9gD6VFSNyluwc3',
-  bucket: 'baishiup',
+  ak: '',
+  sk: '',
+  bucket: '',
 };
